64d88e35b56ef39964b0cda4637cdb0c29b0f4f2
Hermetheus <allanmolson@gmail.com>
2021-09-07 18:20:43 -0500

    IJSRuntime to Invoke Javascript Functions

--- BlazorMovies/Client/Pages/Counter.razor.cs
+++ BlazorMovies/Client/Pages/Counter.razor.cs
@@ -1,12 +1,12 @@
 ﻿using BlazorMovies.Shared.Entities;
 
 using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+
 
 namespace BlazorMovies.Client.Pages
 {
@@ -14,7 +14,7 @@ namespace BlazorMovies.Client.Pages
     {
         [Inject] SingletonService singleton { get; set; }
         [Inject] TransientService transient { get; set; }
-
+[Inject] IJSRuntime js { get; set; }
         private List<Movie> movies;
         protected override void OnInitialized()
         {
@@ -28,12 +28,27 @@ namespace BlazorMovies.Client.Pages
 
 
         private int currentCount = 0;
+        private static int currentCountStatic = 0;
 
-        private void IncrementCount()
+        [JSInvokable]
+        public async Task IncrementCount()
         {
             currentCount++;
             singleton.Value += 1;
             transient.Value += 1;
+            currentCountStatic++;
+            await js.InvokeVoidAsync("dotnetStaticInvocation");
+        }
+
+        private async Task IncrementCountJavaScript()
+        {
+            await js.InvokeVoidAsync("dotnetInstanceInvocation", DotNetObjectReference.Create(this));
+        }
+
+        [JSInvokable]
+        public static Task<int> GetCurrentCount()
+        {
+            return Task.FromResult(currentCountStatic);
         }
     }
 }
